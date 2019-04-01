ce7bc8ee6f674eafbc005264d75c45c6230f252f
Noor Chaudhry <noor.m.chaudhry@gmail.com>
2019-04-01 03:53:22 -0400

    Create New Test Class
    Create the new test class used on the SubTasksController

---
+++ comp2084-assignment1.Tests/Controllers/SubTasksControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using comp2084_assignment1;
+using comp2084_assignment1.Controllers;
+
+namespace comp2084_assignment1.Tests.Controllers
+{
+    [TestClass]
+    public class SubTasksControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            SubTasksController controller = new SubTasksController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
